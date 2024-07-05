b9743585cd38d3dc4506ede9d5e3b822189ca8ae
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-05 11:25:42 +0300

    Updated Card component JSX and CSS, added _button.scss file"

--- class-notes/rc03-SASS/src/components/card/Card.jsx
+++ class-notes/rc03-SASS/src/components/card/Card.jsx
@@ -4,26 +4,43 @@ import "./Card.scss";
 const Card = () => {
   return (
     <div className="container">
-    {
-        veri.map(item => {
-            return(
-                <div className="card">
-                <h1>{item.name}</h1>
-                <h2>{item.job}</h2>
-                <p>{item.comment}</p>
-                <img src={item.img} alt="user" />
-                <div className="btn-div">
-                    <button className="btn-div--small">Small</button>
-                    <button className="btn-div--large">Large</button>
-                </div>
-
-                </div>
-                
-            )
-        })
-    }
+      {veri.map(({ name, job, comment, img, id }) => {
+        return (
+          <div className="card" key={id}>
+            <h1>{name} </h1>
+            <h2>{job} </h2>
+            <p>{comment} </p>
+            <img src={img} alt="user" />
+            <div className="btn-div">
+              <button className="btn-div--small">Small</button>
+              <button className="btn-div--large">Large</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
+export default Card;
 
-export default Card
+// const Card = () => {
+//     return (
+//       <div className="container">
+//         {veri.map((item) => {
+//           return (
+//             <div className="card" key={item.id}>
+//               <h1>{item.name} </h1>
+//               <h2>{item.job} </h2>
+//               <p>{item.comment} </p>
+//               <img src={item.img} alt="user" />
+//               <div className="btn-div">
+//                 <button className="btn-div--small">Small</button>
+//                 <button className="btn-div--large">Large</button>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   };
+//   export default Card;
