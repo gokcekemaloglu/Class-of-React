202fb712c84c6018838d4c13bbc5d87a79f52105
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-08 13:20:21 +0300

    Updated Products component: added React import, modified JSX, and added stylesheet"

--- class-notes/rc04-ProductList/src/components/products/Products.jsx
+++ class-notes/rc04-ProductList/src/components/products/Products.jsx
@@ -1,28 +1,29 @@
-import ProductCard from "./ProductCard";
+import React from 'react'
+import ProductCard from './ProductCard'
+import "./Products.scss"
 
-const Products = ({ products }) => {
-  // console.log(products);
+const Products = ({products}) => {
+   
   return (
-    <div className="urun-list">
+    <div className="ürün-list">
+
+      {/*//! Buradan spreadle açarak gönderip objenin propertylerini props objesine yerleştirme */}
       {products.map((ürün) => (
-        <div>
-          {/*//! Buradan spreadle açarak gönderip objenin propertylerini props objesine yerleştirme */}
-          <ProductCard {...ürün}/>
-
-          {/* üç nokta destruct içinde destruct yapılmasını engelliyor, tek seferde hallediyoruz */}
-
-          {/*//! Best Practice Alt Componente Gönderme */}
-          {/*//! Buradan açmadan gönderip alt componentte karşılayıp içini açma */}
-
-          {/* <ProductCard ürün={ürün}/> */}
-
-          {/* bütün diziyi bu sayfada bastırabilirdik, ancak best practise burada mapla dönüp başka sayfada bastrmak */}
-          {/* <h3>{ürün.price}</h3>
-          <img src={ürün.image} alt="" /> */}
-        </div>
+        <ProductCard key={ürün.id} {...ürün} />
       ))}
+
+      {/*//! Best Practice Alt Componente Gönderme */}
+      {/*//! Buradan açmadan gönderip alt componentte karşılayıp içini açma */}
+      {/* {products.map((ürün) => (
+        <ProductCard key={ürün.id} ürün={ürün} />
+      ))} */}
+
+    
+      
+   
+      
     </div>
   );
-};
+}
 
-export default Products;
+export default Products
