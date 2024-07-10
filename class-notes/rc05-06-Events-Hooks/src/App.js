90ba745241ae9639fc14910bfe776c83d1404676
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-10 11:15:03 +0300

    Added Form component and updated App.js to render it instead of Counter component

--- class-notes/rc05-06-Events-Hooks/src/App.js
+++ class-notes/rc05-06-Events-Hooks/src/App.js
@@ -1,6 +1,7 @@
 import Events from "./1-events-hooksIntro/Events";
 import Hooks from "./2-hooks/Hooks";
 import Counter from "./3-classComponents/Counter";
+import Form from "./4-form/Form";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <Events/> */}
       {/* <Hooks/> */}
-      <Counter/>
+      {/* <Counter/> */}
+      <Form/>
       
     </div>
   );
