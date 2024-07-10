800689f3763eb143cfa0decf8c0adefb1d44fdba
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-10 13:24:09 +0300

    Updated Form component with event handling and state updates; added new components to App.js"

---
+++ class-notes/rc05-06-Events-Hooks/src/4-form/FormObject.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+
+const FormObject = () => {
+    const [info, setInfo] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+    const {name, email, password} = info
+
+
+    // State kalabalığını engellemek için genele vererek handleInfo'yu oluşturduk
+    const handleInfo = (e) => {
+        console.log(e.target.id);
+        setInfo({...info, [e.target.id]: e.target.value})
+        console.log(e);
+    }
+
+    const sendToDatabase = (e) => {
+        e.preventDefault()
+        alert(`
+            name : ${name}
+            password : ${password}
+            email : ${email}
+            
+            `);
+    }
+    
+  return (
+    <div className="mt-4 p-3">
+      <div className="text-center">
+        <h1>*********************************</h1>
+        <h2>FORM EVENTS</h2>
+      </div>
+      <form onSubmit={sendToDatabase}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name: <span className="text-danger">{name} </span>
+          </label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="name" 
+            required 
+            onChange={handleInfo}
+            value={name}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email: <span className="text-danger">{} </span>
+          </label>
+          <input 
+            type="email" 
+            className="form-control" 
+            id="email" 
+            required  
+            onChange={handleInfo}
+            value={email}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password: <span className="text-danger">{} </span>
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            required
+            onChange={handleInfo}
+            value={password}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormObject;
--- class-notes/rc05-06-Events-Hooks/src/App.js
+++ class-notes/rc05-06-Events-Hooks/src/App.js
@@ -2,6 +2,8 @@ import Events from "./1-events-hooksIntro/Events";
 import Hooks from "./2-hooks/Hooks";
 import Counter from "./3-classComponents/Counter";
 import Form from "./4-form/Form";
+import FormObject from "./4-form/FormObject";
+import MouseEvents from "./5-mouse-events/MouseEvents";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,7 +13,9 @@ function App() {
       {/* <Events/> */}
       {/* <Hooks/> */}
       {/* <Counter/> */}
-      <Form/>
+      {/* <Form/> */}
+      {/* <FormObject/> */}
+      <MouseEvents/>
       
     </div>
   );
