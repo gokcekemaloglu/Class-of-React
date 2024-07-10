6a01b9b85d07cc0da38604613fd867d90334c00f
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-10 11:54:58 +0300

    Added password, country, and remember fields to Form component, along with corresponding state variables and event handlers. Implemented sendToDatabase function to handle form submission.

--- class-notes/rc05-06-Events-Hooks/src/4-form/Form.jsx
+++ class-notes/rc05-06-Events-Hooks/src/4-form/Form.jsx
@@ -2,6 +2,22 @@ import React, { useState } from "react";
 
 const Form = () => {
     const [name, setName] = useState("");
+    const [password, setPassword] = useState("")
+    const [country, setCountry] = useState("")
+    const [remember, setRemember] = useState("")
+
+    const sendToDatabase = () => {
+      alert(`
+        name : ${name}
+        password : ${password}
+        country : ${country}
+        remember : ${remember}
+        `);
+        setName("")
+        setPassword("")
+        setCountry("")
+        setRemember("")
+    }
 
 
   return (
@@ -10,17 +26,17 @@ const Form = () => {
         <h1>*********************************</h1>
         <h2>FORM EVENTS</h2>
       </div>
-      <form>
+      <form ac onSubmit={sendToDatabase}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
-            Name: <span> {name} </span>
+            Name: <span className="text-danger">{name} </span>
           </label>
           <input 
-          type="text" 
-          className="form-control" 
-          id="name" 
-          aria-describedby="emailHelp" 
-          onChange={(e)=>setName(e.target.value)}
+            type="text" 
+            className="form-control" 
+            id="name" 
+            aria-describedby="emailHelp" 
+            onChange={(e)=>setName(e.target.value)}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
@@ -28,18 +44,40 @@ const Form = () => {
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
-            Password
+            Password: <span className="text-danger">{password}</span>
           </label>
-          <input type="password" className="form-control" id="password"/>
+          <input 
+            type="password" 
+            className="form-control" 
+            id="password" 
+            onChange={(e)=>setPassword(e.target.value)}                
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="country" className="form-label">
+            Country: <span className="text-danger"> {country} </span>
+          </label>
+          <select 
+          id="country"
+          className="form-select"
+          onChange={(e)=>setCountry(e.target.value)}
+          >
+            <option >COUNTRIES</option>
+            <option value="Türkiye">TURKEY</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select>
+          
         </div>
         <div className="mb-3 form-check">
           <input
             type="checkbox"
             className="form-check-input"
             id="remember"
+            onChange={(e) => setRemember(e.target.checked)}
           />
           <label className="form-check-label" htmlFor="remember">
-            Remember
+            Remember: <span className="text-danger">{remember.toString()}</span>
           </label>
         </div>
         <button type="submit" className="btn btn-primary">
