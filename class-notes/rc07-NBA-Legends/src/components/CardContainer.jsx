5a82db46cd3bf6997d45bc87a61b0d4a5d159db7
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-12 13:13:22 +0300

    Added search functionality to CardContainer, toggling image/statistics display on Player card click, and updated CSS styles for player cards and container.

--- class-notes/rc07-NBA-Legends/src/components/CardContainer.jsx
+++ class-notes/rc07-NBA-Legends/src/components/CardContainer.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../helper/data'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Form, Row } from 'react-bootstrap'
 import Player from './Player'
 
 const CardContainer = () => {
     // console.log(data);
 
+    const [arama, setArama] = useState("")
+
+    // react alanında set'i kullanılmayacaksa tekrar useState'e gerek yok
+
+    //! 1.Yol
+    // let filtrelenmisData = ""
+
+    // filtrelenmisData = data.filter((a)=>a.name.includes(arama))
+
+    // console.log(filtrelenmisData);
+
   return (
     <>
-        {/* input açılacak */}
+        <Form.Control className='w-50 m-auto' type="search" placeholder="Search player..." onChange={(e)=>setArama(e.target.value)}/>
 
-        <Container>
-            <Row>
-                {data.map((player)=>(
-                    <Col key={player.name}>
+        <Container className='kart-container my-4 p-3 rounded-4'>
+            <Row className='g-3'>
+                {/* {filtrelenmisData.map((player)=>( */}
+                {data.filter((abc)=>abc.name.toLowerCase().includes(arama.toLowerCase())).map((player)=>(
+                    <Col key={player.name} md={6} lg={4} xl={3}>
                         <Player {...player}/>
                     </Col>
                 ))}
--- class-notes/rc07-NBA-Legends/src/components/Player.jsx
+++ class-notes/rc07-NBA-Legends/src/components/Player.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 
 const Player = ({name, img, statistics}) => {
+
+    const [showResim, setShow] = useState(true)
+
   return (
-    <Card className='rounded-2 m-auto player-kart' role="button">
-        <Card.Img className='player-resim' variant="top" src={img} />
+    <Card onClick={()=>setShow(!showResim)} className='rounded-2 m-auto player-kart' role="button">
+
+        {showResim ? (
+            <Card.Img className='player-resim' variant="top" src={img} />
+        ) : (
+            <ul className='m-auto'>
+                {statistics.map((item,index)=>(
+                    <li key={index} className='h5 list-unstyled text-start'>🏀 {item}</li>
+                ))}
+            </ul>
+        )}  
+      
+
 
         <Card.Footer>
-        <Card.Title>{name}</Card.Title>
+            <Card.Title>{name}</Card.Title>
         </Card.Footer>
     </Card>
   )
