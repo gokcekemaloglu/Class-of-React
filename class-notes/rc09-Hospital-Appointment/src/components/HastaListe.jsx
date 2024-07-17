cd2d9f80ae2628aeca6a3db64e5a27a2b2ca599d
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-17 15:56:02 +0300

    Updated HastaListe component: added deleteHasta function, modified filtering logic, and added onClick event to FaTimesCircle icon

--- class-notes/rc09-Hospital-Appointment/src/components/HastaListe.jsx
+++ class-notes/rc09-Hospital-Appointment/src/components/HastaListe.jsx
@@ -3,9 +3,25 @@ import { FaTimesCircle } from "react-icons/fa";
 
 const HastaListe = ({hastalar, setHastalar, doktorlar }) => {
 
-  // (doktorlar.length === 1) ? setHastalar(hastalar.filter((a)=> a.myDoctor === doktorlar[0].doctorName)) : hastalar
+  // if(doktorlar.length===1){
+  //   setHastalar(hastalar.filter((a)=> a.myDoctor===doktorlar[0].doctorName))
+  // }else setHastalar(hastalar)
+  // üstteki gibi yazarsam doktorlar dizisi teke düştüğünde sonsuz döngü onblur, çünkü 1 doktor varken setHastalar a git diyoruz, gidip buraya döndüğünde doktor sayısı 1 old için yine set çalışır
+
+  // başka yol
+  // doktorlar.length === 4 ? (ilk yaptığımız gibi hastalarda gez hepsini bastır) : 
+
+  // doktorlar[0].doktor === doktorum && (soldaki şarta uyanları bastır)
+
+  //son yol da alttaki doktorlar 4 olsa da bir olsa da ismi geçen doktorun hastalarını bastır
+  // hastalarda gezerken ilk hastayı al, bişey bastırmadan doktorlarda gezin, elindeki hastanın doktoru,o anki doktorlar dizisinde varsa, o hastayı bastır. 4 doktor varken bütün hastalar basılır. tek doktor varken, bahsi geçen doktorun hastaları bastırılır
+
+  const deleteHasta = (id) => {
+    setHastalar(hastalar.filter((x)=>x.id !== id))
+  }
 
   return (
+
     <div>
       {hastalar.map((hasta)=>(
         <div key={hasta.id}>
@@ -19,7 +35,7 @@ const HastaListe = ({hastalar, setHastalar, doktorlar }) => {
                 <h3>{hasta.myDoctor}</h3>
               </div>
               <div>
-                <FaTimesCircle style={{color:"red"}} />
+                <FaTimesCircle style={{color:"red"}} onClick={()=>deleteHasta(hasta.id)}/>
               </div>
 
             </div>
