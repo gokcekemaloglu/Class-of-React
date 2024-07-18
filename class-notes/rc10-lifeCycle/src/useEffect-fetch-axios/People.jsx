9cca92610a6cc23f20256879a4da3a04265f3f11
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-18 13:32:19 +0300

    Added comments in People.jsx

--- class-notes/rc10-lifeCycle/src/useEffect-fetch-axios/People.jsx
+++ class-notes/rc10-lifeCycle/src/useEffect-fetch-axios/People.jsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const People = () => {
-
-    const[people,setPeople] = useState([])
-
-    
+    const[people,setPeople] = useState([])    
 
     //! 1. Yol fetch then yapısı
 
@@ -50,6 +47,9 @@ const People = () => {
         <div className='row'>
             {people.map(({id,name,username,phone})=>(
                 <div className='col-12 col-sm-6 col-md-4 col-lg-3' key={id}>
+                {/* https://www.dicebear.com/styles/avataaars/ */}
+
+
                     <img src={`https://api.dicebear.com/9.x/avataaars/svg?seed=${name}`} alt="" />
 
                     <h5>{name}</h5>
