900da5aae1e9618a3104938229cbcc87ed9b62bf
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-18 10:48:29 +0300

    updated

---
+++ class-notes/rc10-lifeCycle/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import LifeCycleMethods from "./LifeCycleMethods";
+
+function App() {
+
+  const [show, setShow] = useState(true)
+  return (
+    <div className="text-center container mt-4">
+
+      <button onClick={()=>setShow(!show)} className="btn btn-danger">SHOW</button>
+
+      {show && <LifeCycleMethods/>} 
+      
+    </div>
+  );
+}
+
+export default App;
