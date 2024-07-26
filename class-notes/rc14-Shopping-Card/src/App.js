d4d518686ad5fdbbd2e94caded18a0fed6cfddd6
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-26 12:12:26 +0300

    Updated App.js, CardTotal.jsx, ProductCard.jsx, index.css, Main.jsx, ProductList.jsx, and AppRouter.jsx files with new code and modifications.

--- class-notes/rc14-Shopping-Card/src/App.js
+++ class-notes/rc14-Shopping-Card/src/App.js
@@ -1,6 +1,8 @@
 
 import AppRouter from "./router/AppRouter"
 
+// index.html ye bootstrap in script teşekkürü ile fontAwesome ın teşekkür link ini yapıştırdık
+
 
 function App() {
   return (
--- class-notes/rc14-Shopping-Card/src/router/AppRouter.jsx
+++ class-notes/rc14-Shopping-Card/src/router/AppRouter.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar"
+import Main from "../pages/Main"
+import NewProduct from '../pages/NewProduct';
+import ProductList from '../pages/ProductList';
+import About from '../pages/About';
 
 
 const AppRouter = () => {
   return <Router>
     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Main/>} />
+      <Route path="/new-product" element={<NewProduct/>} />
+      <Route path="/products" element={<ProductList/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/" element={<Main/>} />
+
+    </Routes>
   </Router>;
 }
 
