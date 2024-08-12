1ed5318a398d1e06c7ee59a6b28e72db1eaf9262
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-08-12 11:28:15 +0300

    "Added React Context API to App.js, created StudentContext, and implemented context provider and consumer in App.js, Home.jsx, and OgrenciItem.jsx

--- class-notes/rc15-Context-Reducer/context-1/src/App.js
+++ class-notes/rc15-Context-Reducer/context-1/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import Home from './components/Home'
+import data from "./data"
+
+//! 1-Context alanı açıyoruz (kısa yol ama best practise değil)
+
+export const StudentContext = createContext()
 
 const App = () => {
+
+  const [students, setStudents] = useState(data)
+
+  const changeColor = (id, newRenk) => {
+    setStudents(students.map((a)=>a.id===id ? {...a, color:newRenk} : a))
+  }
+  
   return (
-    <div>App</div>
+
+    //! 2-Bütün projeye gönderilmek üzere ilk Home sayfasını(componentini)gönderilecek verilerle sarmalladık
+    <StudentContext.Provider value={{students, changeColor}}>
+
+      <Home/>
+
+    </StudentContext.Provider>
   )
 }
 
--- class-notes/rc15-Context-Reducer/context-1/src/components/OgrenciItem.jsx
+++ class-notes/rc15-Context-Reducer/context-1/src/components/OgrenciItem.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StudentContext } from '../App'
 
 const OgrenciItem = () => {
+
+  //! 3- (context consumer) globalde (mesela App.js'de) tanımladığım değerleri buraya çağırdık
+  const {students, changeColor} = useContext(StudentContext)
+  // console.log(students);
+  
   return (
-    <div>OgrenciItem</div>
+    <div>
+      {students.map((i)=>(
+        <div key={i.id} style={{backgroundColor: i.color}}>
+          <h3>NAME: {i.name}</h3>
+          <h4>E-MAİL: {i.email}</h4>
+          <h4>AGE: {i.age}</h4>
+          Color: <input type="text" value={i.color}
+            onChange={(e)=>changeColor(i.id,e.target.value)}
+          />
+        </div>
+      ))}
+    </div>
   )
 }
 
