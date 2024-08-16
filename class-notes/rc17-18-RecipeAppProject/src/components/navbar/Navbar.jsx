0a28f6d09803643f7796b1574a243ac3ac59bd16
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-08-16 12:19:37 +0300

    Updated Navbar component with new styles and functionality, added About page, and modified AppRouter to include Navbar, About, and NotFound routes.

--- class-notes/rc17-18-RecipeAppProject/src/components/navbar/Navbar.jsx
+++ class-notes/rc17-18-RecipeAppProject/src/components/navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyles'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react';
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
   return (
-    <div>Navbar</div>
+    <Nav>
+      <Logo to="/home">
+        <i>{"<Clarusway/>"}</i>
+        <span>recipe</span>
+      </Logo>
+      <Hamburger onClick={()=>setOpen(!open)}>
+        <GiHamburgerMenu/>
+      </Hamburger>
+
+      <Menu osman={open} onClick={()=>setOpen(!open)}>
+        <MenuLink to="/about">About</MenuLink>
+        {/* <MenuLink to="/">Github</MenuLink> */}
+        <a href="https://github.com/" target="blank">Github</a>
+        <MenuLink to="/">Logout</MenuLink>
+      </Menu>
+
+    </Nav>
   )
 }
 
--- class-notes/rc17-18-RecipeAppProject/src/router/AppRouter.jsx
+++ class-notes/rc17-18-RecipeAppProject/src/router/AppRouter.jsx
@@ -4,11 +4,15 @@ import Login from '../pages/login/Login'
 import Home from '../pages/home/Home'
 import PrivateRouter from "./PrivateRouter"
 import Details from '../pages/details/Details'
+import Navbar from '../components/navbar/Navbar'
+import About from '../pages/about/About'
+import Footer from '../components/footer/Footer'
+import NotFound from '../pages/errorPage/NotFound'
 
 const AppRouter = () => {
   return (
     <Router>
-      {/* navbar */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Login/>} />
         
@@ -16,8 +20,10 @@ const AppRouter = () => {
           <Route path="/home" element={<Home/>}/>
         </Route>
         <Route path="/details" element={<Details/>}/>
-
+        <Route path="/about" element={<About/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
