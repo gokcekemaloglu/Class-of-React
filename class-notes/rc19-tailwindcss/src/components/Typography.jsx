70e1a4d31147a911c4316d136f9f659471b7ad8a
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-08-19 12:14:24 +0300

    Added Typography import and component, and commented out a div with bg-slate-600 and h-screen classes

---
+++ class-notes/rc19-tailwindcss/src/components/Typography.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Typography = () => {
+  return (
+    <div>
+        <h3 className="text-2xl ">Typography</h3>
+        <p className="text-sm">Typography</p>
+        <p className="text-lg">Typography</p>
+        <p className="text-xl">Typography</p>
+        <p className="text-3xl">Typography</p>
+        <p className="text-4xl">Typography</p>
+        <p className="text-6xl">Typography</p>
+        <p className="text-[75px]">Typography</p>
+        <p className="text-[10rem]">Typography</p>
+        <article>
+            <p className="line-clamp-3 hover:line-clamp-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facilis similique, esse voluptatibus doloremque, asperiores quod minus quae sunt nesciunt corrupti aut nisi, explicabo sequi delectus vero commodi repudiandae fuga provident. Modi adipisci consequatur dolorum repellat impedit neque commodi voluptatum ullam cum, quam fugiat nesciunt ex sapiente quae mollitia sit eveniet perspiciatis nemo maiores, quaerat exercitationem? Est architecto incidunt tempora totam dignissimos iusto autem quod unde. Voluptatum nihil veniam recusandae iure veritatis aliquam sunt ducimus. Eligendi, quis voluptatum alias dignissimos officia voluptatem, nesciunt nulla reprehenderit dolore unde adipisci perspiciatis dolor. Voluptatibus eligendi beatae aut praesentium perspiciatis in, obcaecati iusto quos voluptatum eos sit vero doloremque officiis aliquid debitis blanditiis nemo voluptate odit quae sint nobis natus iste ipsam! Voluptate, tempore dignissimos recusandae vero illum vel laudantium minus hic sequi, nisi quaerat suscipit accusamus, itaque sapiente. Illum maxime non voluptate dolor corporis. Unde in molestiae minus atque delectus non reprehenderit harum?</p>
+        </article>
+
+        <p className="text-center">Typography</p>
+        <p className="underline">Typography</p>
+        <p className="line-through">Typography</p>
+        <p className="text-red-300">Typography</p>
+        <p className="text-green-500">Typography</p>
+        <p className="text-6xl text-[rgb(154,200,205)]">Typography</p>
+        <p className="text-6xl text-[rgb(154,200,5)]">Typography</p>
+        <p className="text-6xl text-[#1E0A32]">Typography</p>
+        <p className="bg-[#b5a5c5] text-white">Typography</p>
+    </div>
+    
+  )
+}
+
+export default Typography
