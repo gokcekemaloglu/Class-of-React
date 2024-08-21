a6f0105121c9de1fb5a97e6b2343e41bc983b531
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-08-21 13:46:28 +0300

    Updated AuthContext to include signIn function, added toast notifications, and modified Login and Register pages to handle user input and authentication

--- class-notes/rc20-Movie-App/movie-app-inclass/src/context/AuthContext.js
+++ class-notes/rc20-Movie-App/movie-app-inclass/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react'
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../auth/firebase';
 import { toastError, toastSuccess } from '../helpers/ToastNotify';
 import { useNavigate } from 'react-router-dom';
@@ -14,10 +14,22 @@ const AuthContext = ({children}) => {
 
   //! register için sitede zincir yapılı fetch işlemi var. Biz burada async await'i tercih edelim dedik, iki kullanımı da görmemiz için.
 
-  const createUser = async(email, password) => {
+  const createUser = async(email, password, displayName) => {
     try {
       await createUserWithEmailAndPassword(auth, email, password)
       toastSuccess("Register Successfully")
+      navigate("/")
+    } catch (error) {
+      toastError(`${error.message} yanlış girdiniz`)
+    }    
+  }
+
+  //! login
+
+  const signIn = async(email,password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      toastSuccess("Logged in Successfully")
       navigate("/")
     } catch (error) {
       toastError(`${error.message} yanlış girdiniz`)
@@ -26,7 +38,7 @@ const AuthContext = ({children}) => {
   }
 
   return (
-    <AuthContextt.Provider value={{createUser}}>
+    <AuthContextt.Provider value={{createUser, signIn}}>
       {children}
     </AuthContextt.Provider>
   )
