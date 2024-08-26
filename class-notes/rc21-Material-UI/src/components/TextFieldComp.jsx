34b4b56e8c870a48a17912be1e648ba52d88bcc4
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-08-26 12:14:42 +0300

    Updated index.html, added Material-UI components to TextFieldComp.jsx, TypoButtons.jsx, and Home.jsx

--- class-notes/rc21-Material-UI/src/components/TextFieldComp.jsx
+++ class-notes/rc21-Material-UI/src/components/TextFieldComp.jsx
@@ -1,8 +1,79 @@
+import { Box, Container, MenuItem, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const TextFieldComp = () => {
+
+  //? aşağıdaki inputun durumu true yada false'a göre değişiyor
+  let errorr = true
+
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
+
   return (
-    <div>TextFieldComp</div>
+    <div>
+      <Container sx={{marginTop:6, display:"flex", flexDirection:"column"}}>
+        <Typography variant="h4">
+          TEXT-FIELD (input)
+        </Typography>
+        <Box sx={{mt:4}}>
+          {/* margin="dense"=  az bir margin verir */}
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="email giriniz..."
+            margin="dense"
+          />
+          <TextField
+          required
+          id="filled-required"
+          label="Filled"
+          defaultValue="Hello World"
+          variant="filled"
+          margin="normal"
+          />
+          <TextField
+            error={errorr}
+            id="filled-error-helper-text"
+            label="Error"
+            defaultValue="Hello World"
+            helperText={errorr && "yanlış girdiniz"}
+            variant="filled"
+          />
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Select"
+            defaultValue="EUR"
+            helperText="Please select your currency"
+            margin="normal"
+          >
+            {/* https://mui.com/material-ui/react-select/ */}
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+      </Container>
+    </div>
   )
 }
 
--- class-notes/rc21-Material-UI/src/components/TypoButtons.jsx
+++ class-notes/rc21-Material-UI/src/components/TypoButtons.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {Box, Button, Container, Typography} from '@mui/material';
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const TypoButtons = () => {
   return (
-    <div>TypoButtons</div>
+    <div>
+      <Container maxWidth="md" >
+        {/* maxWidth="md" width en fazla md büyüklükte olsun.  gutterBottom={true}
+        alttan margin. component attribute ü  sadece console da etkilidir , görüntü yine variant neyse odur*/}
+        <Typography variant="h1" component="h3" gutterBottom align="center" color="error" sx={{background:"#eee", mt:4, border:"1px solid red"}}>
+          Typography Container Box Button
+        </Typography>
+        {/* body1: p etiketidir,body1=16px daha büyük fs ,body2=14px daha küçük font lu.  button=span etiketidir+büyük harf */}
+        <Typography variant="button">
+          span olacak Lorem, ipsum.
+        </Typography>
+        <Typography variant="body1">
+          p etiketi olacak Lorem ipsum dolor sit amet consectetur.
+        </Typography>
+        <Typography variant="body2">
+          p etiketi olacak Lorem ipsum dolor sit amet consectetur.
+        </Typography>
+      </Container>
+
+      <Box sx={{display:"flex", flexDirection:"column", marginTop:4, gap:2}}>
+        <Button variant="outlined" color="error" startIcon={<DeleteIcon/>}>DELETE</Button>
+        <Button variant="contained" color="success" sx={{color:"lightblue"}} endIcon={<SendIcon />}>SEND</Button>
+      </Box>
+    </div>
   )
 }
 
