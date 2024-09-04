d3fc733ecbbeb893d785dd105b44f471d347d04f
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-04 12:17:37 +0300

    Added new routes for Home, Products, Firms, Brands, Purchases, and Sales pages in AppRouter

--- class-notes/rc25-30-stock-app/inclass-stock-app/src/router/AppRouter.jsx
+++ class-notes/rc25-30-stock-app/inclass-stock-app/src/router/AppRouter.jsx
@@ -3,6 +3,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Firms from "../pages/Firms";
+import Brands from "../pages/Brands";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
 
 const AppRouter = () => {
   return (
@@ -11,7 +17,17 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />        
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            {/* <Route path="" element={<Home/>}/> */}
+            <Route index element={<Home/>}/>
+            <Route path="brands" element={<Brands/>}/>
+            {/* <Route path="/stock/brands" element={<Brands/>}/> bu, Absolute path diğer yazış şekli relative path oluyor */}
+            <Route path="products" element={<Products/>}/>
+            <Route path="firms" element={<Firms/>}/>
+            <Route path="purchases" element={<Purchases/>}/>
+            <Route path="sales" element={<Sales/>}/>
+          </Route>
+
         </Route>
       </Routes>
     </Router>
